a86c3236eb3ac2cf708bbccf384d911404920d6c
Vicente Soler <vicente@visose.com>
2020-01-23 17:09:04 +0000

    Input number

--- ExamplePlugin/ExampleCommand.cs
+++ ExamplePlugin/ExampleCommand.cs
@@ -18,6 +18,26 @@ namespace ExamplePlugin
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
+            var get = new Rhino.Input.Custom.GetNumber();
+            get.SetCommandPrompt("Select length");
+
+            double length = 30;
+
+            while (true)
+            {
+                var result = get.Get();
+
+                if (get.CommandResult() != Result.Success)
+                    return get.CommandResult();
+
+                if (result == Rhino.Input.GetResult.Number)
+                {
+                    length = get.Number();
+                    RhinoApp.WriteLine($"Number is: {length}");
+                    break;
+                }
+            }
+
             var objects = doc.Objects.FindByObjectType(ObjectType.Curve);
 
             doc.Objects.Delete(_baked, true);
@@ -26,9 +46,9 @@ namespace ExamplePlugin
             foreach (var obj in objects)
             {
                 var curve = obj.Geometry as Curve;
-                var extrusions = Brep.CreateFromTaperedExtrude(curve, 3, Vector3d.ZAxis, curve.PointAtStart, Math.PI*0.25, ExtrudeCornerType.None, doc.ModelAbsoluteTolerance, doc.ModelAngleToleranceRadians);
+                var extrusions = Brep.CreateFromTaperedExtrude(curve, length, Vector3d.ZAxis, curve.PointAtStart, Math.PI * 0.25, ExtrudeCornerType.None, doc.ModelAbsoluteTolerance, doc.ModelAngleToleranceRadians);
 
-                foreach(var extrusion in extrusions)
+                foreach (var extrusion in extrusions)
                 {
                     var guid = doc.Objects.AddBrep(extrusion);
                     _baked.Add(guid);
@@ -36,7 +56,7 @@ namespace ExamplePlugin
             }
 
             doc.Views.Redraw();
-          //  RhinoApp.WriteLine("Goodbye world.");
+            //  RhinoApp.WriteLine("Goodbye world.");
             return Result.Success;
         }
     }
