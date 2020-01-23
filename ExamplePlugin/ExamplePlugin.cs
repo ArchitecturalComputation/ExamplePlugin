cfe7d017cce745b8a87f710cd082caf48dc3192d
Vicente Soler <vicente@visose.com>
2020-01-23 15:01:50 +0000

    Add project files.

---
+++ ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,16 @@
+﻿using Rhino.PlugIns;
+using System;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("4DFF4085-EB3D-4B5C-AF55-704BFB239977")]
+
+namespace ExamplePlugin
+{
+    public class ExamplePlugin : PlugIn
+    {
+        public ExamplePlugin()
+        {
+
+        }
+    }
+}
