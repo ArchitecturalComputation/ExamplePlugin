69881d4964cc157b41d86e63e60cdb5ea24734b6
Vicente Soler <vicente@visose.com>
2020-01-23 15:33:43 +0000

    Build automation

---
+++ ExamplePlugin/ExampleCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino;
+using Rhino.Commands;
+
+namespace ExamplePlugin
+{
+    public class ExampleCommand : Command
+    {
+        public override string EnglishName => "ExampleCommand";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("Hello world");
+            return Result.Success;
+        }
+    }
+}
