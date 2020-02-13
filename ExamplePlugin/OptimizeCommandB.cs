5a611d35b0147cc3ab53ed6fabfd52ee12951b1c
Vicente Soler <vicente@visose.com>
2020-02-13 12:35:51 +0000

    RandomSearch end

--- ExamplePlugin/OptimizeCommandB.cs
+++ ExamplePlugin/OptimizeCommandB.cs
@@ -9,6 +9,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ExamplePlugin
 {
@@ -22,10 +23,6 @@ namespace ExamplePlugin
             go.SetCommandPrompt("Optimize B");
             go.AddOption("Run");
 
-            var conduit = new ConduitB();
-            conduit.Enabled = true;
-            doc.Views.Redraw();
-
             while (true)
             {
                 go.Get();
@@ -33,14 +30,40 @@ namespace ExamplePlugin
                 if (go.CommandResult() != Result.Success)
                     return go.CommandResult();
 
-                if(go.Option().EnglishName == "Run")
+                if (go.Option().EnglishName == "Run")
                 {
                     RhinoApp.WriteLine("Running optimization...");
                     break;
                 }
             }
 
-            conduit.Enabled = false;
+            var optimize = new OptimizerB(50, 10);
+
+            var conduit = new ConduitB();
+            conduit.Enabled = true;
+            conduit.Path = optimize.Path;
+            conduit.Info = optimize.Info;
+
+            Task redrawTask = null;
+
+            optimize.UpdateCandidate = () =>
+            {
+                conduit.Path = optimize.Path;
+                conduit.Info = optimize.Info;
+
+                if (redrawTask?.IsCompleted != false)
+                    redrawTask = Task.Run(doc.Views.Redraw);
+            };
+
+            optimize.Finished = () =>
+             {
+                 RhinoApp.WriteLine(optimize.Info);
+                 doc.Objects.AddPolyline(optimize.Path);
+                 doc.Views.Redraw();
+                 conduit.Enabled = false;
+             };
+
+            Task.Run(optimize.RandomSearch);
 
             return Result.Success;
         }
@@ -48,16 +71,24 @@ namespace ExamplePlugin
 
     class ConduitB : DisplayConduit
     {
+        public Polyline Path { get; set; }
+        public string Info { get; set; }
+
         protected override void CalculateBoundingBox(CalculateBoundingBoxEventArgs e)
         {
             var bbox = new BoundingBox(0, 0, 0, 10, 10, 10);
             e.IncludeBoundingBox(bbox);
         }
 
+        protected override void DrawForeground(DrawEventArgs e)
+        {
+            var position = new Point2d(100, 100);
+            e.Display.Draw2dText(Info, Color.White, position, false, 24);
+        }
+
         protected override void PostDrawObjects(DrawEventArgs e)
         {
-            var c = new Circle(new Point3d(5, 5, 0), 4);
-            e.Display.DrawCircle(c, Color.White);
+            e.Display.DrawPolyline(Path, Color.White);
         }
     }
 }
--- ExamplePlugin/OptimizerB.cs
+++ ExamplePlugin/OptimizerB.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ExamplePlugin
 {
@@ -13,13 +14,21 @@ namespace ExamplePlugin
         public string Info { get; private set; }
 
         Random _random = new Random(42);
+        int _timeLimit;
         Point3d[] _cities;
         int[] _order;
+        double _currentFitness;
+        int _betterCandidateCount;
+        int _iterations;
+        public Action UpdateCandidate;
+        public Action Finished;
 
-        public OptimizerB(int cities, double size = 10.0)
+        public OptimizerB(int cities, int timeLimit = 10, double size = 10.0)
         {
+            _timeLimit = timeLimit * 1000;
             _cities = RandomCities(cities, size).ToArray();
             _order = Enumerable.Range(0, cities).ToArray();
+            UpdateDisplay();
         }
 
         IEnumerable<Point3d> RandomCities(int count, double size)
@@ -34,6 +43,13 @@ namespace ExamplePlugin
         void UpdateDisplay()
         {
             Path = new Polyline(_order.Select(i => _cities[i]));
+            Info = $"Fitness: {_currentFitness:0.0} ({++_betterCandidateCount}/{_iterations})";
+        }
+
+        void UpdateCanditate()
+        {
+            UpdateDisplay();
+            UpdateCandidate.Invoke();
         }
 
         double Fitness(int[] candidate)
@@ -51,12 +67,6 @@ namespace ExamplePlugin
             return totalDistance;
         }
 
-        //void Suffle(int[] candidate)
-        //{
-        //    Array.Sort(candidate, new Comparison<int>((i, j) => _random.Next(0, 1) * 2 - 1));
-        //    var shuffled = candidate.OrderBy(i => _random.NextDouble()).ToArray();
-        //}
-
         void Shuffle(int[] list)
         {
             for (int i = list.Length - 1; i >= 1; i--)
@@ -73,15 +83,28 @@ namespace ExamplePlugin
             b = temp;
         }
 
-        void RandomSearch()
+        public void RandomSearch()
         {
-            int count = 1000;
-            double currentFitness = Fitness(_order);
+            _currentFitness = Fitness(_order);
 
-            while (count-- > 0)
+            var watch = Stopwatch.StartNew();
+
+            while (watch.ElapsedMilliseconds < _timeLimit)
             {
+                Shuffle(_order);
+                double fitness = Fitness(_order);
 
+                if (fitness < _currentFitness)
+                {
+                    _currentFitness = fitness;
+                    UpdateCanditate();
+                }
+
+                _iterations++;
             }
+
+            Finished.Invoke();
         }
+
     }
 }
